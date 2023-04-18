d6f27640050b5b3dc80a4dd7829cca3da67aeed0
Henrique Fonseca <henrique@fonseca.email>
2023-04-18 19:31:58 +0100

    Update GuestGuard.tsx

--- src/features/auth/components/GuestGuard.tsx
+++ src/features/auth/components/GuestGuard.tsx
@@ -9,26 +9,37 @@ import LoadingScreen from 'src/components/loading-screen';
 //
 import { useAuthContext } from 'src/contexts/useAuthContext';
 
+
 // ----------------------------------------------------------------------
+
 
 type GuestGuardProps = {
   children: React.ReactNode;
 };
 
 export default function GuestGuard({ children }: GuestGuardProps) {
+
   const { isAuthenticated, isInitialized } = useAuthContext();
 
   if (!isInitialized) {
     return <LoadingScreen />;
   }
 
+
   if (isAuthenticated) {
+
+   
+
     const { pathname, push } = useRouter();
 
     useEffect(() => {
       push(PATHS.companies.root);
     }, [pathname, push]);
+
+    return <LoadingScreen />;
   }
 
-  return <LoadingScreen />;
+
+
+  return <>{children}</>;
 }
