9d955cce395d5d9980740e284040cfe6c44cd685
Bernardo-Bento <bernardo.bento@careplace.pt>
2023-04-18 22:31:40 +0100

    Checkpoint

--- src/features/account/components/AccountMenu.tsx
+++ src/features/account/components/AccountMenu.tsx
@@ -1,14 +1,20 @@
- 
 // next
 import NextLink from 'next/link';
+
+// auth
+import { useAuthContext } from 'src/contexts';
 // @mui
 import { alpha } from '@mui/material/styles';
+import Modal from '@mui/material/Modal';
 import {
   Link,
   Stack,
   Drawer,
   Avatar,
   Divider,
+  Button,
+  Box,
+  Typography,
   ListItemIcon,
   ListItemText,
   ListItemButton,
@@ -16,40 +22,47 @@ import {
 // hooks
 import useResponsive from 'src/hooks/useResponsive';
 import useActiveLink from 'src/hooks/useActiveLink';
+import { useEffect, useState } from 'react';
 // config
 import { NAV } from 'src/layouts';
 // routes
 import { PATHS } from 'src/routes/paths';
+import { useRouter } from 'next/router';
 // _mock
 import _mock from 'src/_mock';
 // components
 import Iconify from 'src/components/iconify';
 import TextMaxLine from 'src/components/text-max-line';
+import LoadingScreen from 'src/components/loading-screen/LoadingScreen';
 
 // ----------------------------------------------------------------------
 
 const navigations = [
   {
-    title: 'Informação Pessoal',
+    title: 'Dados Pessoais',
     path: PATHS.account.personal,
-    icon: <Iconify icon="carbon:user" />,
+    icon: <Iconify icon="material-symbols:account-circle" />,
   },
   {
     title: 'Familiares',
     path: PATHS.account.relatives,
-    icon: <Iconify icon="carbon:favorite" />,
+    icon: <Iconify icon="material-symbols:family-restroom-rounded" />,
   },
   {
     title: 'Pedidos',
     path: PATHS.account.orders,
-    icon: <Iconify icon="carbon:document" />,
+    icon: <Iconify icon="material-symbols:reorder-rounded" />,
   },
   {
-    title: 'Pagamentos',
+    title: 'Informações de Pagamento',
     path: PATHS.account.payment,
-    icon: <Iconify icon="carbon:purchase" />,
+    icon: <Iconify icon="ic:round-payment" />,
   },
-  
+  {
+    title: 'Definições',
+    path: PATHS.account.settings,
+    icon: <Iconify icon="material-symbols:settings-outline-rounded" />,
+  },
 ];
 
 // ----------------------------------------------------------------------
@@ -61,6 +74,16 @@ type Props = {
 
 export default function AccountMenu({ open, onClose }: Props) {
   const isMdUp = useResponsive('up', 'md');
+  const { user, logout } = useAuthContext();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const routes = useRouter();
+
+  const handleLogoutClick = () => {
+    setIsLoading(true);
+    logout();
+    routes.push(PATHS.home);
+  };
 
   const renderContent = (
     <Stack
@@ -74,25 +97,87 @@ export default function AccountMenu({ open, onClose }: Props) {
         }),
       }}
     >
+      <Modal open={openModal} onClose={() => setOpenModal(false)}>
+        <Box
+          sx={{
+            outline: 'none',
+            width: '500px',
+            height: 'auto',
+            bgcolor: 'white',
+            borderRadius: '16px',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'flex-start',
+            padding: '20px',
+            gap: '10px',
+          }}
+        >
+          <Typography variant="h6">Alterar Imagem de Perfil</Typography>
+          <Divider sx={{ width: '100%' }} />
+          <Box
+            sx={{
+              width: '100%',
+              height: 'auto',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'flex-start',
+              padding: '10px',
+              gap: '10px',
+            }}
+          >
+            <Avatar
+              src={user?.profile_picture}
+              sx={{ width: '100px', height: '100px', mb: '50px' }}
+            />
+            <Box
+              sx={{
+                width: '100%',
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'flex-end',
+                gap: '20px',
+              }}
+            >
+              <Button
+                variant="outlined"
+                sx={{ width: '100%', height: '50px', color: 'red', borderColor: 'red' }}
+              >
+                Cancelar
+              </Button>
+              <Button variant="contained" sx={{ width: '100%', height: '50px' }}>
+                Guardar
+              </Button>
+            </Box>
+          </Box>
+        </Box>
+      </Modal>
       <Stack spacing={2} sx={{ p: 3, pb: 2 }}>
         <Stack spacing={2} direction="row" alignItems="center">
-          <Avatar src={_mock.image.avatar(0)} sx={{ width: 64, height: 64 }} />
+          <Avatar src={user?.profile_picture} sx={{ width: 64, height: 64 }} />
           <Stack
             direction="row"
             alignItems="center"
+            onClick={() => setOpenModal(true)}
             sx={{ typography: 'caption', cursor: 'pointer', '&:hover': { opacity: 0.72 } }}
           >
             <Iconify icon="carbon:edit" sx={{ mr: 1 }} />
-            Change photo
+            Alterar imagem
           </Stack>
         </Stack>
 
         <Stack spacing={0.5}>
           <TextMaxLine variant="subtitle1" line={1}>
-            Jayvion Simon
+            {user?.name}
           </TextMaxLine>
           <TextMaxLine variant="body2" line={1} sx={{ color: 'text.secondary' }}>
-            nannie_abernathy70@yahoo.com
+            {user?.email}
           </TextMaxLine>
         </Stack>
       </Stack>
@@ -119,7 +204,8 @@ export default function AccountMenu({ open, onClose }: Props) {
             <Iconify icon="carbon:logout" />
           </ListItemIcon>
           <ListItemText
-            primary="Logout"
+            onClick={handleLogoutClick}
+            primary="Sair"
             primaryTypographyProps={{
               typography: 'body2',
             }}
@@ -129,7 +215,9 @@ export default function AccountMenu({ open, onClose }: Props) {
     </Stack>
   );
 
-  return (
+  return isLoading ? (
+    <LoadingScreen />
+  ) : (
     <>
       {isMdUp ? (
         renderContent
@@ -162,8 +250,8 @@ type MenuItemProps = {
 };
 
 function MenuItem({ item }: MenuItemProps) {
-  const { active } = useActiveLink(item.path);
-
+  const { pathname } = useRouter();
+  const active = pathname === item.path;
   return (
     <Link
       component={NextLink}
