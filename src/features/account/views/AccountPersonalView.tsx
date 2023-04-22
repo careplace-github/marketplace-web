25f0409c0fa5395f17fb17faeb139daf703cbe6f
Bernardo-Bento <bernardo.bento@careplace.pt>
2023-04-22 22:02:35 +0100

    checkpoint (pre build)

--- src/features/account/views/AccountPersonalView.tsx
+++ src/features/account/views/AccountPersonalView.tsx
@@ -23,15 +23,10 @@ import { AccountLayout } from '../components';
 
 // ----------------------------------------------------------------------
 
-const GENDER_OPTIONS = ['Male', 'Female', 'Other'];
-
-// ----------------------------------------------------------------------
-
 export default function AccountPersonalView() {
-  const [showPassword, setShowPassword] = useState(false);
   const [openModal, setOpenModal] = useState(false);
   const isMdUp = useResponsive('up', 'md');
-  const { user } = useAuthContext();
+  const { user, updateUser } = useAuthContext();
   const theme = useTheme();
 
   const AccountPersonalSchema = Yup.object().shape({
@@ -45,9 +40,9 @@ export default function AccountPersonalView() {
     city: Yup.string().required('A cidade é obrigatória'),
     zipCode: Yup.string().required('O código postal é obrigatório'),
   });
-  const firstName = user?.name ? user.name.split(' ')[0] : null;
+
   const defaultValues = {
-    firstName,
+    firstName: user?.name ? user.name.split(' ')[0] : null,
     lastName: user?.name ? user.name.split(' ').pop() : null,
     emailAddress: user?.email ? user.email : null,
     phoneNumber: user?.phone ? user.phone : null,
@@ -62,13 +57,8 @@ export default function AccountPersonalView() {
     confirmNewPassword: '',
   };
 
-  const [newData, setNewData] = useState(defaultValues);
-
-  useEffect(() => {
-    console.log(firstName);
-  }, [firstName]);
-
   const methods = useForm<typeof defaultValues>({
+    mode: 'onChange',
     resolver: yupResolver(AccountPersonalSchema),
     defaultValues,
   });
@@ -76,14 +66,23 @@ export default function AccountPersonalView() {
   const {
     reset,
     handleSubmit,
-    formState: { isSubmitting },
+    formState: { isSubmitting, isDirty },
   } = methods;
 
   const onSubmit = async (data: typeof defaultValues) => {
     try {
       await new Promise((resolve) => setTimeout(resolve, 500));
-      reset();
-      console.log('DATA', data);
+      user.name = data.firstName + ' ' + data.lastName;
+      user.email = data.emailAddress;
+      user.phone = data.phoneNumber;
+      user.birthdate = data.birthday;
+      user.gender = data.gender;
+      user.address.street = data.streetAddress;
+      user.address.postal_code = data.zipCode;
+      user.address.city = data.city;
+      user.address.country = data.country;
+      updateUser(user);
+      reset(data);
     } catch (error) {
       console.error(error);
     }
@@ -91,120 +90,131 @@ export default function AccountPersonalView() {
 
   return (
     <AccountLayout>
-      <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-        <Typography variant="h5" sx={{ mb: 3 }}>
-          Dados Pessoais
-        </Typography>
-        {!isMdUp && (
-          <Box
-            sx={{
-              width: '100%',
-              pb: '40px',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
-              gap: '15px',
-            }}
-          >
-            <UploadPictureModal open={openModal} onClose={() => setOpenModal(false)} />
-            <Avatar
-              onClick={() => setOpenModal(true)}
-              src={user?.profile_picture}
-              sx={{ width: '150px', height: '150px' }}
-            />
-            <Stack
-              direction="row"
-              alignItems="center"
-              onClick={() => setOpenModal(true)}
-              sx={{ typography: 'caption', cursor: 'pointer', '&:hover': { opacity: 0.72 } }}
+      <Box
+        sx={{
+          p: 3,
+          bgcolor: 'white',
+          borderRadius: '16px',
+          boxShadow:
+            'rgba(145, 158, 171, 0.2) 0px 0px 2px 0px, rgba(145, 158, 171, 0.12) 0px 12px 24px -4px;',
+        }}
+      >
+        <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+          <Typography variant="h5" sx={{ mb: 3 }}>
+            Dados Pessoais
+          </Typography>
+          {!isMdUp && (
+            <Box
+              sx={{
+                width: '100%',
+                pb: '40px',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                gap: '15px',
+              }}
             >
-              <Iconify icon="carbon:edit" sx={{ mr: 1 }} />
-              Alterar imagem
-            </Stack>
-          </Box>
-        )}
-        <Box
-          rowGap={2.5}
-          columnGap={2}
-          display="grid"
-          gridTemplateColumns={{ xs: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
-        >
-          <RHFTextField name="firstName" label="Nome" />
-
-          <RHFTextField name="lastName" label="Apelido" />
-
-          <RHFTextField name="emailAddress" label="Email" />
-
-          <RHFPhoneField
-            name="phoneNumber"
-            label="Telemóvel"
-            defaultCountry="PT"
-            forceCallingCode
-          />
-
-          <Controller
-            name="birthday"
-            render={({ field, fieldState: { error } }) => (
-              <DatePicker
-                format="dd MMM yyyy"
-                label="Data de nascimento"
-                slotProps={{
-                  textField: {
-                    helperText: error?.message,
-                    error: !!error?.message,
-                  },
-                }}
-                {...field}
-                value={new Date(field.value)}
+              <UploadPictureModal open={openModal} onClose={() => setOpenModal(false)} />
+              <Avatar
+                onClick={() => setOpenModal(true)}
+                src={user?.profile_picture}
+                sx={{ width: '150px', height: '150px' }}
               />
-            )}
-          />
-
-          <RHFSelect native name="gender" label="Género">
-            {genders.map((option) => (
-              <option key={option.value} value={option.value}>
-                {option.label}
-              </option>
-            ))}
-          </RHFSelect>
-
-          <RHFTextField name="streetAddress" label="Morada" />
-
-          <RHFTextField name="zipCode" label="Código Postal" />
-
-          <RHFTextField name="city" label="Cidade" />
-
-          <RHFSelect native name="country" label="País">
-            <option value="" />
-            {countries.map((country) => (
-              <option key={country.code} value={country.code}>
-                {country.label}
-              </option>
-            ))}
-          </RHFSelect>
-        </Box>
-        <Box sx={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}>
-          <LoadingButton
-            sx={{
-              mt: '20px',
-              backgroundColor: 'primary.main',
-              color: theme.palette.mode === 'light' ? 'common.white' : 'grey.800',
-              '&:hover': {
-                backgroundColor: 'primary.dark',
-                color: theme.palette.mode === 'light' ? 'common.white' : 'grey.800',
-              },
-            }}
-            color="inherit"
-            size="large"
-            type="submit"
-            variant="contained"
-            loading={isSubmitting}
+              <Stack
+                direction="row"
+                alignItems="center"
+                onClick={() => setOpenModal(true)}
+                sx={{ typography: 'caption', cursor: 'pointer', '&:hover': { opacity: 0.72 } }}
+              >
+                <Iconify icon="carbon:edit" sx={{ mr: 1 }} />
+                Alterar imagem
+              </Stack>
+            </Box>
+          )}
+          <Box
+            rowGap={2.5}
+            columnGap={2}
+            display="grid"
+            gridTemplateColumns={{ xs: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
           >
-            Save Changes
-          </LoadingButton>
-        </Box>
-      </FormProvider>
+            <RHFTextField name="firstName" label="Nome" />
+
+            <RHFTextField name="lastName" label="Apelido" />
+
+            <RHFTextField name="emailAddress" label="Email" />
+
+            <RHFPhoneField
+              name="phoneNumber"
+              label="Telemóvel"
+              defaultCountry="PT"
+              forceCallingCode
+            />
+
+            <Controller
+              name="birthday"
+              render={({ field, fieldState: { error } }) => (
+                <DatePicker
+                  format="dd-MM-yyyy"
+                  label="Data de nascimento"
+                  slotProps={{
+                    textField: {
+                      helperText: error?.message,
+                      error: !!error?.message,
+                    },
+                  }}
+                  {...field}
+                  value={new Date(field.value)}
+                />
+              )}
+            />
+
+            <RHFSelect native name="gender" label="Género">
+              {genders.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </RHFSelect>
+
+            <RHFTextField name="streetAddress" label="Morada" />
+
+            <RHFTextField name="zipCode" label="Código Postal" />
+
+            <RHFTextField name="city" label="Cidade" />
+
+            <RHFSelect native name="country" label="País">
+              <option value="" />
+              {countries.map((country) => (
+                <option key={country.code} value={country.code}>
+                  {country.label}
+                </option>
+              ))}
+            </RHFSelect>
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}>
+            <LoadingButton
+              sx={{
+                mt: '20px',
+                backgroundColor: 'primary.main',
+                color: theme.palette.mode === 'light' ? 'common.white' : 'grey.800',
+                '&:hover': {
+                  backgroundColor: 'primary.dark',
+                  color: theme.palette.mode === 'light' ? 'common.white' : 'grey.800',
+                },
+              }}
+              color="inherit"
+              disabled={!isDirty}
+              size="large"
+              type="submit"
+              variant="contained"
+              loading={isSubmitting}
+            >
+              Save Changes
+            </LoadingButton>
+          </Box>
+        </FormProvider>
+      </Box>
     </AccountLayout>
   );
 }
