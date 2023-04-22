f4cdde719c992ab43ba2c33edf8c3a4379913e8f
Henrique Fonseca <henrique.fonseca@careplace.pt>
2023-04-22 16:23:16 +0100

    Added register page

---
+++ src/components/hook-form/RHFPhoneField.tsx
@@ -0,0 +1,62 @@
+// form
+import { useFormContext, Controller } from 'react-hook-form';
+// @mui
+import { TextField, TextFieldProps } from '@mui/material';
+import { MuiTelInput } from 'mui-tel-input';
+
+import styled from 'styled-components' 
+// ----------------------------------------------------------------------
+
+type Props = TextFieldProps & {
+  phone: string;
+};
+
+/**
+ * @see https://viclafouch.github.io/mui-tel-input/docs/css/
+ */
+// Country code font size should be the same has the input font size
+const MuiTelInputStyled = styled(MuiTelInput)`
+
+.MuiTelInput-Input {
+  font-size: 0.875rem;
+}
+
+
+.MuiTelInput-Typography-calling-code {
+  font-size: 0.875rem;
+
+  &.MuiTypography-root {
+    font-size: 0.875rem;
+  }
+
+
+}
+
+
+`
+
+/**
+ * @see https://viclafouch.github.io/mui-tel-input/docs/api-reference/
+ */
+export default function RHFPhoneField({ name, helperText, ...other }: Props) {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState: { error } }) => (
+        <MuiTelInputStyled
+          {...field}
+          fullWidth
+          value={typeof field.value === 'number' && field.value === 0 ? '' : field.value}
+          error={!!error}
+          helperText={error ? error?.message : helperText}
+          // Change the font size of the country code
+
+          {...other}
+        />
+      )}
+    />
+  );
+}
--- src/features/auth/components/AuthRegisterForm.tsx
+++ src/features/auth/components/AuthRegisterForm.tsx
@@ -1,4 +1,6 @@
 import * as Yup from 'yup';
+import React from 'react';
+import { MuiTelInput } from 'mui-tel-input';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useState } from 'react';
@@ -8,39 +10,81 @@ import { Typography, Stack, Link, IconButton, InputAdornment } from '@mui/materi
 import { useTheme } from '@mui/material/styles';
 // components
 import Iconify from 'src/components/iconify';
-import FormProvider, { RHFTextField } from 'src/components/hook-form';
+import FormProvider, { RHFTextField, RHFPhoneField } from 'src/components/hook-form';
+import { useSnackbar } from 'src/components/snackbar';
+// routes
+import { PATHS } from 'src/routes/paths';
+// auth
+import { useAuthContext } from 'src/contexts';
+// data
+import { countries } from 'src/data';
+// types
+import { IUserProps } from 'src/types/user';
 
 // ----------------------------------------------------------------------
 
 type FormValuesProps = {
   email: string;
   password: string;
+  name: string;
+  phone: string;
+  confirmPassword: string;
 };
 
 export default function AuthRegisterForm() {
+  const { enqueueSnackbar } = useSnackbar();
+
+  const { register } = useAuthContext();
+
   const theme = useTheme();
-  
+
   const [showPassword, setShowPassword] = useState(false);
 
   const RegisterSchema = Yup.object().shape({
-    fullName: Yup.string()
-      .required('Full name is required')
-      .min(6, 'Mininum 6 characters')
-      .max(15, 'Maximum 15 characters'),
-    email: Yup.string().required('Email is required').email('That is not an email'),
+    name: Yup.string()
+      .required('O nome é obrigatório.')
+      .min(3, 'O nome deve ter pelo menos 3 caracteres.')
+      .max(50, 'O nome deve ter no máximo 50 caracteres.'),
+    email: Yup.string()
+      .required('O email é obrigatório.')
+      .email('O email introduzido não é válido.'),
     password: Yup.string()
-      .required('Password is required')
-      .min(6, 'Password should be of minimum 6 characters length'),
+      .required('A password é obrigatória.')
+      .min(6, 'A password deve ter pelo menos 6 caracteres.')
+      .max(50, 'A password deve ter no máximo 50 caracteres.'),
     confirmPassword: Yup.string()
-      .required('Confirm password is required')
-      .oneOf([Yup.ref('password')], "Password's not match"),
+      .required('A confirmação da password é obrigatória.')
+      .oneOf([Yup.ref('password')], 'As passwords não coincidem.'),
+    phone: Yup.string()
+
+      .test('phone', 'O número de telemóvel é obrigatório', (value) => {
+        // If the value is equal to a country phone number, then it is empty
+        const code = countries.find((country) => country.phone === value.replace('+', ''))?.phone;
+        const phone = value.replace('+', '');
+
+        return code !== phone;
+      })
+
+      .test('phone', 'O número de telemóvel introduzido não é válido.', (value) => {
+        // Portuguese phone number verification
+        if (value.startsWith('+351')) {
+          // Remove spaces and the +351 sign
+          value = value.replace(/\s/g, '').replace('+351', '');
+
+          // Check if the phone number is valid
+          return value.length === 9;
+        }
+
+        return true;
+      }),
   });
 
   const defaultValues = {
-    fullName: '',
+    name: '',
     email: '',
     password: '',
     confirmPassword: '',
+    phone: '+351',
   };
 
   const methods = useForm<FormValuesProps>({
@@ -56,10 +100,35 @@ export default function AuthRegisterForm() {
 
   const onSubmit = async (data: FormValuesProps) => {
     try {
-      await new Promise((resolve) => setTimeout(resolve, 500));
-      reset();
-      console.log('DATA', data);
+      // Get the country from the /data/countries.ts file by the phone number
+      const countryCode = (countries.find((country) => country.phone === data.phone.slice(0, data.phone.indexOf(' ')).replace('+', '')) as any).code;
+     // get the frst character unil the first space eg: +351 123 456 789 => +351
+      
+
+     console.log("countryCode",countryCode)
+
+      // Remove spaces from the phone number
+      const phone = data.phone.replace(/\s/g, '');
+
+
+
+      // Create the user object
+
+      const user: IUserProps = {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+        phone: phone,
+        address: {
+          country: countryCode,
+        },
+      };
+
+      await register(user);
+
+      enqueueSnackbar('Conta criada com sucesso.', { variant: 'success' });
     } catch (error) {
+      enqueueSnackbar('Erro ao criar conta. Por favor tente novamente', { variant: 'error' });
       console.error(error);
     }
   };
@@ -67,9 +136,18 @@ export default function AuthRegisterForm() {
   return (
     <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
       <Stack spacing={2.5}>
-        <RHFTextField name="fullName" label="Full Name" />
+        <RHFTextField name="name" label="Nome" />
 
-        <RHFTextField name="email" label="Email address" />
+        <RHFTextField name="email" label="Email" />
+
+        <RHFPhoneField
+          name="phone"
+          label="Telemóvel"
+          focusOnSelectCountry
+          defaultCountry="PT"
+          forceCallingCode
+          flagSize="small"
+        />
 
         <RHFTextField
           name="password"
@@ -79,7 +157,7 @@ export default function AuthRegisterForm() {
             endAdornment: (
               <InputAdornment position="end">
                 <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
-                  <Iconify icon={showPassword ? 'carbon:view' : 'carbon:view-off'}/>
+                  <Iconify icon={showPassword ? 'carbon:view' : 'carbon:view-off'} />
                 </IconButton>
               </InputAdornment>
             ),
@@ -88,13 +166,13 @@ export default function AuthRegisterForm() {
 
         <RHFTextField
           name="confirmPassword"
-          label="Confirm Password"
+          label="Confirmar Password"
           type={showPassword ? 'text' : 'password'}
           InputProps={{
             endAdornment: (
               <InputAdornment position="end">
                 <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
-                  <Iconify icon={showPassword ? 'carbon:view' : 'carbon:view-off'}/>
+                  <Iconify icon={showPassword ? 'carbon:view' : 'carbon:view-off'} />
                 </IconButton>
               </InputAdornment>
             ),
@@ -110,30 +188,36 @@ export default function AuthRegisterForm() {
           loading={isSubmitting}
           sx={{
             px: 4,
+
+            mb: 30,
+
             bgcolor: 'primary.main',
             color: theme.palette.mode === 'light' ? 'common.white' : 'grey.800',
             '&:hover': {
               bgcolor: 'primary.dark',
-              color: 
-                theme.palette.mode === 'light' ? 'common.white' : 'grey.800',
+              color: theme.palette.mode === 'light' ? 'common.white' : 'grey.800',
             },
           }}
         >
-          Register
+          Criar Conta
         </LoadingButton>
 
         <Typography variant="caption" align="center" sx={{ color: 'text.secondary', mt: 3 }}>
-          {`I agree to `}
-          <Link color="text.primary" href="#" underline="always">
-            Terms of Service
+          {`Ao criar conta declara que leu e aceita os nossos `}
+          <Link
+            color="text.primary"
+            href={PATHS.termsAndConditions}
+            underline="always"
+            target="_blank"
+          >
+            Termos e Condições
           </Link>
-          {` and `}
-          <Link color="text.primary" href="#" underline="always">
-            Privacy Policy.
+          {` e a `}
+          <Link color="text.primary" href={PATHS.privacyPolicy} underline="always" target="_blank">
+            Política de Privacidade.
           </Link>
         </Typography>
       </Stack>
     </FormProvider>
   );
 }
-
