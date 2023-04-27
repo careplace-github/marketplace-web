da6d6bc6b14e688f7bdfda82d2a07a0e9a1538d9
Henrique Fonseca <henrique@fonseca.email>
2023-04-27 21:24:09 +0100

    Checkpoint

--- src/components/hook-form/RHFTextField.tsx
+++ src/components/hook-form/RHFTextField.tsx
@@ -19,6 +19,7 @@ export default function RHFTextField({ name, helperText, ...other }: Props) {
       render={({ field, fieldState: { error } }) => (
         <TextField
           {...field}
+          maxLength={3}
           fullWidth
           value={typeof field.value === 'number' && field.value === 0 ? '' : field.value}
           error={!!error}
