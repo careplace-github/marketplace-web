55690531fc7bbe996f7364915b3bacbde1a56909
Henrique Fonseca <henrique.fonseca@careplace.pt>
2023-05-02 12:46:04 +0100

    Update kinshipDegrees.ts

--- src/data/kinshipDegrees.ts
+++ src/data/kinshipDegrees.ts
@@ -1,10 +1,20 @@
 const kinshipDegrees = [
     { value: 'father', label: 'Pai' },
-    { value: 'grandFather', label: 'Avô' },
     { value: 'mother', label: 'Mãe' },
-    { value: 'grandMother', label: 'Avó' },
+
+    { value: 'grandfather', label: 'Avô' },  
+    { value: 'grandmother', label: 'Avó' },
+    { value: 'greatGrandfather', label: 'Bisavô' },
+    { value: 'greatGrandmother', label: 'Bisavó' },
+     
+    { value: 'uncle', label: 'Tio' },
+    { value: 'aunt', label: 'Tia' },
     { value: 'son', label: 'Filho' },
     { value: 'daughter', label: 'Filha' },
+     
+    { value: 'brother', label: 'Irmão' },
+    { value: 'sister', label: 'Irmã' },
+
     { value: 'other', label: 'Outro' },
   ];
  export default kinshipDegrees; 
