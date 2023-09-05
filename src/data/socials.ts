822c8e91c13216575ab61bef58085c0b916c2b70
Henrique Fonseca <henrique@fonseca.email>
2023-09-05 22:22:38 +0100

    add social links

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,12 +1,12 @@
 const socials = [
   {
-    value: 'https://www.facebook.com/careplace.pt',
+    value: 'https://www.facebook.com/careplacehealth',
     label: 'Facebook',
     icon: 'carbon:logo-facebook',
     color: '#1877F2',
   },
   {
-    value: 'https://www.instagram.com/careplace.pt',
+    value: 'https://www.instagram.com/careplacehealth',
     label: 'Instagram',
     icon: 'carbon:logo-instagram',
     color: '#E02D69',
