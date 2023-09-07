52e97dd84d32c3f2cf2df1dac35b2cfb9c871b8a
bernardobento <bernardo.bento@careplace.pt>
2023-09-07 19:59:46 +0100

    Resolver rating bug when rating.count === 0

--- src/features/orders/components/questionnaire/OrderQuestionnaireSummary.tsx
+++ src/features/orders/components/questionnaire/OrderQuestionnaireSummary.tsx
@@ -68,11 +68,12 @@ export default function OrderQuestionnaireSummary({
           <Stack spacing={0.5} direction="row" alignItems="center">
             <Iconify icon="carbon:star-filled" sx={{ color: 'warning.main' }} />
             <Box sx={{ typography: 'h6' }}>{company.rating.average.toFixed(1)}</Box>
-            {company.rating.count && (
-              <Link variant="body2" sx={{ color: 'text.secondary' }}>
-                ({company.rating.count} avaliações)
-              </Link>
-            )}
+            {company.rating.count ||
+              (company.rating.count === 0 && (
+                <Link variant="body2" sx={{ color: 'text.secondary' }}>
+                  ({company.rating.count} avaliações)
+                </Link>
+              ))}
           </Stack>
           <Stack
             direction="row"
