68dcccc1cb16f7887de6184cc5f3918483936bf9
bernardobento <bernardo.bento@careplace.pt>
2023-09-07 23:09:59 +0100

    feat: Optimized redirect from /support to /coming-soon

--- src/pages/support.tsx
+++ src/pages/support.tsx
@@ -1,10 +1,5 @@
 // next
 import Head from 'next/head';
-// react
-import { useEffect } from 'react';
-// router
-import { useRouter } from 'next/router';
-import { PATHS } from 'src/routes';
 // layouts
 import MainLayout from 'src/layouts/main';
 // sections
@@ -17,25 +12,12 @@ SupportPage.getLayout = (page: React.ReactElement) => <MainLayout>{page}</MainLa
 // ----------------------------------------------------------------------
 
 export default function SupportPage() {
-  const rediretToComingSoon = true;
-  const router = useRouter();
+  return;
+  <>
+    <Head>
+      <title>Suporte | Careplace</title>
+    </Head>
 
-  useEffect(() => {
-    if (router.isReady && rediretToComingSoon) {
-      router.push(PATHS.comingsoon);
-    }
-  }, [router.isReady]);
-
-  return rediretToComingSoon ? (
-    // eslint-disable-next-line react/jsx-no-useless-fragment
-    <></>
-  ) : (
-    <>
-      <Head>
-        <title>Suporte | Careplace</title>
-      </Head>
-
-      <SupportView />
-    </>
-  );
+    <SupportView />
+  </>;
 }
