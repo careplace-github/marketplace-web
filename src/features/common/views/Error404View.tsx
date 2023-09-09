67032681b88b084d756c9bd6f62089afb023e789
Henrique Fonseca <henrique@fonseca.email>
2023-09-09 12:26:56 +0100

    style: code formatting

--- src/features/common/views/Error404View.tsx
+++ src/features/common/views/Error404View.tsx
@@ -15,37 +15,37 @@ import { PATHS } from 'src/routes/paths';
 export default function Error404View() {
   return (
     <MotionContainer>
-        <m.div variants={varBounce().in}>
-          <Typography variant="h3" paragraph>
-            404 - Página Não Encontrada
-          </Typography>
-        </m.div>
-
-        <m.div variants={varBounce().in}>
-          <Typography sx={{ color: 'text.secondary' }}>
-            Pedimos desculpa mas não conseguimos encontrar a página que procura. Talvez tenha
-            introduzido o URL incorreto?
-          </Typography>
-        </m.div>
-
-        <m.div variants={varBounce().in}>
-          <PageNotFoundIllustration
-            sx={{
-              height: 260,
-              my: { xs: 5, sm: 10 },
-            }}
-          />
-        </m.div>
-
-        <Typography sx={{ color: 'text.secondary', mb: 10 }}>
-          <br />
-          Se achar que isto é um erro por favor contacte-nos através do email{' '}
-          <a href="mailto:suporte@careplace.pt"> suporte@careplace.pt </a>
+      <m.div variants={varBounce().in}>
+        <Typography variant="h3" paragraph>
+          404 - Página Não Encontrada
         </Typography>
+      </m.div>
 
-        <Button component={NextLink} href={PATHS.companies.root} size="large" variant="contained">
-          Início
-        </Button>
-      </MotionContainer>
+      <m.div variants={varBounce().in}>
+        <Typography sx={{ color: 'text.secondary' }}>
+          Pedimos desculpa mas não conseguimos encontrar a página que procura. Talvez tenha
+          introduzido o URL incorreto?
+        </Typography>
+      </m.div>
+
+      <m.div variants={varBounce().in}>
+        <PageNotFoundIllustration
+          sx={{
+            height: 260,
+            my: { xs: 5, sm: 10 },
+          }}
+        />
+      </m.div>
+
+      <Typography sx={{ color: 'text.secondary', mb: 10 }}>
+        <br />
+        Se achar que isto é um erro por favor contacte-nos através do email{' '}
+        <a href="mailto:suporte@careplace.pt"> suporte@careplace.pt </a>
+      </Typography>
+
+      <Button component={NextLink} href={PATHS.companies.root} size="large" variant="contained">
+        Início
+      </Button>
+    </MotionContainer>
   );
 }
--- src/features/payments/views/CheckoutSuccessView.tsx
+++ src/features/payments/views/CheckoutSuccessView.tsx
@@ -14,35 +14,35 @@ import { MotionContainer, varBounce } from 'src/components/animate';
 export default function CheckoutSuccessView() {
   return (
     <Container
-        component={MotionContainer}
-        sx={{
-          textAlign: 'center',
-          pt: { xs: 5, md: 10 },
-          pb: { xs: 10, md: 20 },
-        }}
+      component={MotionContainer}
+      sx={{
+        textAlign: 'center',
+        pt: { xs: 5, md: 10 },
+        pb: { xs: 10, md: 20 },
+      }}
+    >
+      <m.div variants={varBounce().in}>
+        <Iconify icon="bxs:check-circle" sx={{ color: 'green' }} width="100px" height="100px" />
+      </m.div>
+
+      <Stack spacing={1} sx={{ my: 5 }}>
+        <Typography variant="h3">O seu pagamento foi efetuado com sucesso!</Typography>
+
+        <Typography sx={{ color: 'text.secondary' }}>
+          Irá receber um email de confirmação com todos os detalhes do pedido
+        </Typography>
+      </Stack>
+
+      <Button
+        component={NextLink}
+        href="/"
+        size="large"
+        color="inherit"
+        variant="contained"
+        startIcon={<Iconify icon="carbon:chevron-left" />}
       >
-        <m.div variants={varBounce().in}>
-          <Iconify icon="bxs:check-circle" sx={{ color: 'green' }} width="100px" height="100px" />
-        </m.div>
-
-        <Stack spacing={1} sx={{ my: 5 }}>
-          <Typography variant="h3">O seu pagamento foi efetuado com sucesso!</Typography>
-
-          <Typography sx={{ color: 'text.secondary' }}>
-            Irá receber um email de confirmação com todos os detalhes do pedido
-          </Typography>
-        </Stack>
-
-        <Button
-          component={NextLink}
-          href="/"
-          size="large"
-          color="inherit"
-          variant="contained"
-          startIcon={<Iconify icon="carbon:chevron-left" />}
-        >
-          Voltar
-        </Button>
-      </Container>
+        Voltar
+      </Button>
+    </Container>
   );
 }
