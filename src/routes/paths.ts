b6b83de88b38324479b56235b8d1b6bbd3b48c3e
Henrique Fonseca <henrique.fonseca@careplace.pt>
2023-09-09 12:59:24 +0100

    feat(api): add sitemap endpoint

--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -55,8 +55,6 @@ export const PATHS = {
   afterLogin: '/companies',
   maintenance: '/maintenance',
   comingsoon: '/coming-soon',
-  pricing01: '/pricing-01',
-  pricing02: '/pricing-02',
   support: '/support',
   page404: '/404',
   page500: '/500',
