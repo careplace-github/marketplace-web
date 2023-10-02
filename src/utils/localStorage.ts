82697b0d148df3c2e00d136db5a7464f51ab79c3
Henrique Fonseca <henrique@fonseca.email>
2023-10-02 09:19:25 +0100

    feat: add util functions for cookies

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -7,3 +7,22 @@ export const setItem = (key: string, value: string | null) => {
 };
 
 export const getItem = (key: string) => localStorage.getItem(key);
+
+// Example of getCookie implementation
+export function getCookie(name: string): string {
+  let value = '; ' + document.cookie;
+  let parts = value.split('; ' + name + '=');
+  if (parts.length === 2) return parts.pop()!.split(';').shift()!;
+  return '';
+}
+
+// Example of setCookie implementation
+export function setCookie(name: string, value: string, days: number = 7): void {
+  let expires = '';
+  if (days) {
+    let date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = '; expires=' + date.toUTCString();
+  }
+  document.cookie = name + '=' + value + expires + '; path=/';
+}
