8704c1cf4078b474d7304436809050ec05d64e57
bernardobento <bernardo.bento@careplace.pt>
2024-03-14 19:22:18 +0000

    fix: add ? to possibly undefined

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -8,9 +8,9 @@ export default function isObjectEmpty(obj: any): boolean {
 }
 
 export function truncateString(str: string, maxLength: number): string {
-  if (str.length <= maxLength) {
+  if (str?.length <= maxLength) {
     return str;
   }
 
-  return `${str.slice(0, maxLength)}...`;
+  return `${str?.slice(0, maxLength)}...`;
 }
